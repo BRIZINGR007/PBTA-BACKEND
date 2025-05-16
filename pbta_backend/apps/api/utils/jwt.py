4f86a15fb9e97c41ea41fa9384006ce355806101
Nabarka Deb <nabarka.deb@u-next.com>
2025-05-16 21:40:44 +0530

    Added changes for  the backend  ...

--- pbta_backend/apps/api/utils/jwt.py
+++ pbta_backend/apps/api/utils/jwt.py
@@ -3,10 +3,16 @@ import datetime
 from decouple import config
 from rest_framework.response import Response
 
+SECRET_KEY = "63b0a8f92bc010485c392c41d386c28be27b28cdcc1d7a23fb8be6668506ebf7931031824eb913e864e2e0479591fb0cb5decf0f3ef06407038dd8b24c720ceec5249ca4a9c6b7c2431cb81b1aecba8abc904919f389277dead75d03cc14dfd1ce0be985460eacf439215e64e941c898940c10d7dccceeea3b4b25e0e57b87cf71b108d8b53cfe1d892228ca6a9caff52b16f57ca93d32b7037b2a78e471d1062abc7e4e2eef4a440afc64dc6a5b26ccbb3d261ebd0a9ea7ee8d90c767fba9a8303e927ff3f2556d220309d6de3352f3b5af8509be4941c92002596635c7b2a6b794725dac59b88ed285e4c7bf0a2ce2273c0ccba7c333c4d6c8aa347c9a6535"
+
 
 class JwtUtils:
     @staticmethod
-    def create_jwt_token(user):
+    def get_secret_key() -> str:
+        return SECRET_KEY
+
+    @classmethod
+    def create_jwt_token(cls, user):
         payload = {
             "user_id": str(user.user_id),
             "email": user.email,
@@ -14,7 +20,7 @@ class JwtUtils:
             "exp": datetime.datetime.now(datetime.timezone.utc)
             + datetime.timedelta(days=7),
         }
-        secret_key = config("SECRET_KEY")
+        secret_key = cls.get_secret_key()
         token = jwt.encode(payload, secret_key, algorithm="HS256")
 
         return token
